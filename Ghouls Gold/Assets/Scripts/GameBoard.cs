0c07115f313eb630cee70d6db508e748b838d471
Steven Pineda <spineda0199@gmail.com>
2020-01-27 20:29:25 -0500

    Pac-Man Remake: Ghouls Gold

---
+++ Ghouls Gold/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBoard : MonoBehaviour
+{
+    private static int boardWidth = 28;
+    private static int boardHeight = 36;
+
+    private bool didStartDeath = false;
+
+    public int totalPellets = 0;
+    public int score = 0;
+    public int pacManLives = 3;
+
+    public AudioClip death;
+    public AudioClip backgroundAudioNormal;
+    public AudioClip backgroundAudioFrightened;
+
+    public Text playerText;
+    public Text readyText;
+  
+
+    public GameObject[,] board = new GameObject[boardWidth, boardHeight];
+
+    void Start()
+    {
+
+        Object[] objects = GameObject.FindObjectsOfType(typeof(GameObject));
+
+        foreach (GameObject o in objects)
+        {
+            Vector2 pos = o.transform.position;
+
+            if (o.name != "PacMan" && o.name != "Nodes" && o.name != "NonNodes" && o.name != "Maze" && o.name != "Pellets" && o.tag != "Ghost" && o.tag != "ghostHome" && o.name != "Canvas" && o.name != "PlayerText" && o.name != "ReadyText")
+            {
+                if (o.GetComponent<Tile> () != null )
+                {
+                    if (o.GetComponent<Tile> ().isPellet || o.GetComponent<Tile> ().isSuperPellet)
+                    {
+                        totalPellets++;
+                    }
+                }
+                board[(int)pos.x, (int)pos.y] = o;
+            }
+            else
+            {
+                //Debug.Log("Found PacMan at: " + pos);
+            }
+        }
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+
+        //- Hide All Ghosts
+        GameObject[] o = GameObject.FindGameObjectsWithTag("Ghost");
+
+        foreach (GameObject ghost in o)
+        {
+
+            ghost.transform.GetComponent<SpriteRenderer>().enabled = false;
+            ghost.transform.GetComponent<Ghost>().canMove = false;
+        }
+
+        GameObject pacMan = GameObject.Find("PacMan");
+        pacMan.transform.GetComponent<SpriteRenderer>().enabled = false;
+        pacMan.transform.GetComponent<PacMan>().canMove = false;
+
+        StartCoroutine(ShowObjectsAfter(2.25f));
+    }
+
+    IEnumerator ShowObjectsAfter(float delay)
+    {
+
+        yield return new WaitForSeconds(delay);
+
+        GameObject[] o = GameObject.FindGameObjectsWithTag("Ghost");
+
+        foreach (GameObject ghost in o)
+        {
+
+            ghost.transform.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        GameObject pacMan = GameObject.Find("PacMan");
+        pacMan.transform.GetComponent<SpriteRenderer>().enabled = true;
+
+        playerText.transform.GetComponent<Text>().enabled = false;
+
+        StartCoroutine(StartGameAfter(2));
+    }
+
+    IEnumerator StartGameAfter(float delay)
+    {
+
+        yield return new WaitForSeconds(delay);
+
+        GameObject[] o = GameObject.FindGameObjectsWithTag("Ghost");
+
+        foreach (GameObject ghost in o)
+        {
+
+            ghost.transform.GetComponent<Ghost>().canMove = true;
+        }
+
+        GameObject pacMan = GameObject.Find("PacMan");
+        pacMan.transform.GetComponent<PacMan>().canMove = true;
+
+        readyText.transform.GetComponent<Text>().enabled = false;
+
+        transform.GetComponent<AudioSource>().clip = backgroundAudioNormal;
+        transform.GetComponent<AudioSource>().Play();
+    }
+
+    public void StartDeath ()
+    {
+        if (!didStartDeath)
+        {
+            didStartDeath = true;
+
+            GameObject[] o = GameObject.FindGameObjectsWithTag("Ghost");
+
+            foreach (GameObject ghost in o)
+            {
+                ghost.transform.GetComponent<Ghost>().canMove = false;
+            }
+
+            GameObject pacMan = GameObject.Find("PacMan");
+            pacMan.transform.GetComponent<PacMan>().canMove = false;
+
+            pacMan.transform.GetComponent<Animator>().enabled = false;
+
+            transform.GetComponent<AudioSource>().Stop ();
+
+            StartCoroutine(ProcessDeathAfter(2));
+
+        }
+    }
+
+    IEnumerator ProcessDeathAfter (float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GameObject[] o = GameObject.FindGameObjectsWithTag("Ghost");
+
+        foreach (GameObject ghost in o)
+        {
+            ghost.transform.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        StartCoroutine(ProcessDeathAnimation(1.9f));
+    }
+
+    IEnumerator ProcessDeathAnimation (float delay)
+    {
+        GameObject pacMan = GameObject.Find("PacMan");
+
+        pacMan.transform.localScale = new Vector3(1, 1, 1);
+        pacMan.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+        pacMan.transform.GetComponent<Animator>().runtimeAnimatorController = pacMan.transform.GetComponent<PacMan>().deathAnimation;
+        pacMan.transform.GetComponent<Animator>().enabled = true;
+
+        transform.GetComponent<AudioSource> ().clip = death;
+        transform.GetComponent<AudioSource> ().Play();
+
+        yield return new WaitForSeconds(delay);
+
+        StartCoroutine(ProcessRestart(2));
+    }
+
+    IEnumerator ProcessRestart (float delay)
+    {
+        GameObject pacMan = GameObject.Find("PacMan");
+        pacMan.transform.GetComponent<SpriteRenderer>().enabled = false;
+
+        transform.GetComponent<AudioSource>().Stop();
+
+        yield return new WaitForSeconds(delay);
+
+        Restart();
+    }
+
+    public void Restart ()
+    {
+        pacManLives -= 1;
+        GameObject pacMan = GameObject.Find("PacMan");
+        pacMan.transform.GetComponent<PacMan>().Restart();
+
+        GameObject [] o = GameObject.FindGameObjectsWithTag("Ghost");
+
+        foreach (GameObject ghost in o)
+        {
+            ghost.transform.GetComponent<Ghost>().Restart();
+        }
+
+        transform.GetComponent<AudioSource>().clip = backgroundAudioNormal;
+        transform.GetComponent<AudioSource>().Play();
+        didStartDeath = false;
+    }
+
+}
